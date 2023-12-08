07d8937fcd57444b1e98b6b3781b1235e73226a4
StasVenger <stasvenger1@gmail.com>
2023-12-08 20:11:04 +0600

    Changed file name

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { render, RenderPosition } from './render.js';
 import ListFilterView from './view/list-filter-view.js';
-import TripHeaderInfoView from './view/trip-header-info.js';
+import TripHeaderInfoView from './view/trip-header-info-view.js';
 import TripPresenter from './presenter/trip-presenter.js';
 
 const siteTripMainElement = document.querySelector('.trip-main');
